3887d530a940b3a32659347eae03ab2e134fc584
anthony <ac.nicholls@gmail.com>
2021-01-31 15:12:24 -0500

    remove cookie service to make build smaller

--- client/ActivitySignUpPortal/src/app/app.module.ts
+++ client/ActivitySignUpPortal/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivityListComponent } from './pages/activity-list/activity-list.component';
