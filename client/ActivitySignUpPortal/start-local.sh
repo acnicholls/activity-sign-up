3fc8b32c2be92c4bd54e0e5640c317ae8ca28742
anthony <ac.nicholls@gmail.com>
2021-06-10 00:48:13 -0400

    setup staging build

---
+++ client/ActivitySignUpPortal/start-local.sh
@@ -0,0 +1,9 @@
+ng serve \
+    --live-reload=true \
+    --watch=true \
+    --host client \
+    --port 4200 \
+    --configuration=local \
+    --public-host=localhost \
+    --disable-host-check \
+    --poll
